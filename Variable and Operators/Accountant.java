fe1d188301877a035b60d49c906a86179de81e19
Đức Nguyễn <ducnguyen2394@gmail.com>
2021-05-15 08:05:55 +0700

    java variable

---
+++ Variable and Operators/Accountant.java
@@ -0,0 +1,9 @@
+public class Accountant {
+	public static void main(String args[]){
+		float interestRate = 8.5f;
+		float money = 125.5f;
+		float interests = (money * interestRate) / 100;
+		System.out.println("lợi nhuận của khách hàng là: " + interests);
+	}
+	
+}
